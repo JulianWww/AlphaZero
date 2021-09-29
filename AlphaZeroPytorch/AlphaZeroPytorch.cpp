97bf0a77e2c9550842758b72d021a39d0f7d5783
Julian Wandhoven <38176040+JulianWww@users.noreply.github.com>
2021-09-29 22:33:41 +0200

    added printout if cuDNN is available

--- AlphaZeroPytorch/AlphaZeroPytorch.cpp
+++ AlphaZeroPytorch/AlphaZeroPytorch.cpp
@@ -9,6 +9,9 @@ using namespace std;
 int main()
 {
 	std::cout << "started training" << std::endl;
+	if (torch::cuda::cudnn_is_available()) {
+		std::cout << "\33[1;32mcudnn is available\33[0m" << std::endl;	
+	}
 	AlphaZero::ai::train(-1);
 	return 0;
 }
