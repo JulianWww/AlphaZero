5f681c3f4a946a5c2eec49632c3e252f27dfd2c9
julianWww <julian.wandhoven@gmail.com>
2021-10-05 23:15:33 +0200

    hotfix for cuda4cpu

--- AlphaZeroPytorch/toCPU.cpp
+++ AlphaZeroPytorch/toCPU.cpp
@@ -8,8 +8,13 @@ int main()
 	std::cout << "type version ID: ";
 	std::cin >> version;
 
+	std::cout << "building model" << std::endl;
 	AlphaZero::ai::Model model = AlphaZero::ai::Model();
+	std::cout << "loading" << std::endl;
 	model.load_version(version);
+	std::cout << "transfering" << std::endl;
 	model.toCPU();
+	std::cout << "saving" << std::endl;
 	model.save_version(version);
+	std::cout << "done" << std::endl;
 }
