7392b0b46be1480298fa25f5cfffd557ba079886
julianWww <julian.wandhoven@gmail.com>
2021-12-18 21:30:01 +0100

    Update doEloRaiting.cpp

--- AlphaZeroPytorch/doEloRaiting.cpp
+++ AlphaZeroPytorch/doEloRaiting.cpp
@@ -50,7 +50,7 @@ int main()
 	AlphaZero::elo::eloClient elo;
 	std::cout << elo.setElo(0, 100) << std::endl;
 
-	int agent = 1;
+	int agent = 131;
 	while (true)
 	{
 		evaluateAgent(agent, 40, elo, lastAgent, currentAgent);
