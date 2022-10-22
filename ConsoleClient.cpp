915698f026f985d03028f43420c23b201c3a5488
Julian Wandhoven <38176040+JulianWww@users.noreply.github.com>
2022-10-22 17:54:48 +0200

    Added cmake

---
+++ ConsoleClient.cpp
@@ -0,0 +1,36 @@
+﻿// ConsoleClient.cpp : Defines the entry point for the application.
+//
+
+#include <agent.hpp>
+
+using namespace std;
+// play a game betwean two agents
+void playGame(std::shared_ptr<Agents::Agent>agent1, std::shared_ptr<Agents::Agent>agent2, std::shared_ptr<AlphaZero::Game::Game>game)
+{
+	while (!game->state->done)
+	{
+		int action;
+		switch (game->state->player)
+		{
+		case(1):{
+			action = agent1->getAction(game);
+			break;
+		}
+		case(-1): {
+			action = agent2->getAction(game);
+			break;
+		}
+		}
+		game->takeAction(action);
+	}
+}
+
+// call the play game function
+int main()
+{
+	auto game = std::make_shared<AlphaZero::Game::Game>();
+	auto user = std::make_shared<Agents::User>();
+	auto AI = std::make_shared<Agents::RemoteAgent>("35.240.231.50", 12345);
+	playGame(user, AI, game);
+	return 0;
+}
